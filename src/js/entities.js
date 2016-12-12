0ffc29e7dff094f9f587758289a378a0e388b4d4
Victoria Quirante <victoria@limenius.com>
2016-12-12 01:03:25 +0100

    Mapping all items

--- src/js/entities.js
+++ src/js/entities.js
@@ -20,11 +20,200 @@ const entities = {
         }
     },
     '#00ff00' : {
-        name: 'Johnny\s body',
+        name: 'Johnny\'s body',
         actions: {
             LOOK : {
                 text: 'He is dead.\nBetter sort this out before Nick arrives.',
                 timeout: 5000,
+            }
+        }
+    },
+    '#ae289d' : {
+        name: 'telephone',
+        actions: {
+            LOOK : {
+                text: 'It\'s a phone.',
+                timeout: 1000,
+            }
+        }
+    },
+    '#2fb068' : {
+        name: 'bottle of tequila',
+        actions: {
+            LOOK : {
+                text: 'Jose Cuervo Special Gold.',
+                timeout: 1000,
+            }
+        }
+    },
+    '#b32633' : {
+        name: 'coat',
+        actions: {
+            LOOK : {
+                text: 'It\'s a coat.',
+                timeout: 1000,
+            }
+        }
+    },
+    '#9d9d9d' : {
+        name: 'coat',
+        actions: {
+            LOOK : {
+                text: 'It\'s a coat.',
+                timeout: 1000,
+            }
+        }
+    },
+    '#a46422' : {
+        name: 'the door',
+        actions: {
+            LOOK : {
+                text: 'That\'s the door. Ronnie is holding it.',
+                timeout: 1500,
+            }
+        }
+    },
+    '#ffffff' : {
+        name: 'Ronnie',
+        actions: {
+            LOOK : {
+                text: 'That\'s Ronnie. A nice guy, not our brigthest bulb.',
+                timeout: 1000,
+            }
+        }
+    },
+    '#493c2b' : {
+        name: 'shelves',
+        actions: {
+            LOOK : {
+                text: 'Those shelves are full of books.\nI don\'t think any of these guys has ever opened one.',
+                timeout: 3000,
+            }
+        }
+    },
+    '#ea6112' : {
+        name: 'lamp',
+        actions: {
+            LOOK : {
+                text: 'It\'s a pretty cool lamp.',
+                timeout: 2000,
+            }
+        }
+    },
+    '#694fc9' : {
+        name: 'tiny table',
+        actions: {
+            LOOK : {
+                text: 'It just holds the phone.',
+                timeout: 1000,
+            }
+        }
+    },
+    '#a3ce27' : {
+        name: 'Joe',
+        actions: {
+            LOOK : {
+                text: 'He is Joe. He\'s always broke, but he knows how to shake a Martini.',
+                timeout: 1000,
+            }
+        }
+    },
+    '#44891a' : {
+        name: 'Lora',
+        actions: {
+            LOOK : {
+                text: 'That\'s Lys. God put some effort on those cheeckbones.\n I think he cut the budget later\nand forgot to use a the human heart, but oh well.',
+                timeout: 4000,
+            }
+        }
+    },
+    '#c2c879' : {
+        name: 'the bar',
+        actions: {
+            LOOK : {
+                text: 'I could really use a drink.',
+                timeout: 1000,
+            }
+        }
+    },
+    '#4dd030' : {
+        name: 'big sofa',
+        actions: {
+            LOOK : {
+                text: 'If that big sofa could talk...',
+                timeout: 1000,
+            }
+        }
+    },
+    '#eb8931' : {
+        name: 'clock',
+        actions: {
+            LOOK : {
+                text: 'It is not stopped showing the time of the murder, how disappointing.',
+                timeout: 2000,
+            }
+        }
+    },
+    '#f7e26b' : {
+        name: 'Jack',
+        actions: {
+            LOOK : {
+                text: 'Tough guy, our Jack. Never try to steal a cigarette from him.',
+                timeout: 1000,
+            }
+        }
+    },
+    '#1b2632' : {
+        name: 'window',
+        actions: {
+            LOOK : {
+                text: 'Lovely evening out there...',
+                timeout: 1500,
+            }
+        }
+    },
+    '#005784' : {
+        name: 'curtains',
+        actions: {
+            LOOK : {
+                text: 'Those are beautiful curtains.',
+                timeout: 1500,
+            }
+        }
+    },
+    '#31a2f2' : {
+        name: 'chest with drawers',
+        actions: {
+            LOOK : {
+                text: 'It has three drawers.',
+                timeout: 1000,
+            }
+        }
+    },
+    '#2f8cb0' : {
+        name: 'wineglass',
+        actions: {
+            LOOK : {
+                text: 'There is still some wine on it.',
+                timeout: 1000,
+            }
+        }
+    },
+    '#2f484e' : {
+        name: 'Flynn',
+        actions: {
+            LOOK : {
+                text: 'It is said that everyone has a gift. Flynn\'s is not playing the guitar',
+                timeout: 1500,
+            }
+        }
+    },
+    '#e06f8b' : {
+        name: 'Spencer',
+        actions: {
+            LOOK : {
+                text: 'That\'s Spencer. A sensible man. He could bet his mother in a poker hand,\nbut we all have our issues...',
+                timeout: 4000,
             }
         }
     },
